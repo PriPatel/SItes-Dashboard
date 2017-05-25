9d04fa6d0c473254b9057728de9c37b9da1cb44a
Pri <pridawg@Pridawgs-MacBook-Pro.local>
2017-05-25 18:22:37 +0100

    News

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
 
 
-$(function()
-{
-	$('.site-cards').jScrollPane();
-});
-	
+
+var stage = $(".stage");
+var existingStages = $(".existing-stages");
+
+
+var newStageButton = $("#new-stage");
+
+
+function addNewStage() { 
+	existingStages.append(stage);
+}
+ 
+
+
+newStageButton.onclick = addNewStage ();
+
+
