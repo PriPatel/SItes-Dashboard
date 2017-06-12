9a0dcfafa57d89dada65b0a395a4ebfc0b5b27ba
PriPatel <priyanka.patel@unrulygroup.com>
2017-06-12 23:31:48 +0100

    new

---
+++ adding.js
@@ -0,0 +1,3 @@
+ $("#btnAddAddress").click(function () {
+      $("#container").append('<div class="address"><div class="input-reg rb-item input-group"><span class="input-group-addon">Address </span>');
+    });
